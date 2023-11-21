f6d2f43580650bdd874aa39d4d429c25661f555c
johnmuthua <caveomuthama@gmail.com>
2023-11-21 17:47:38 +0300

    Add correct functionality to the program

--- 2015/dayone.c
+++ 2015/dayone.c
@@ -1,27 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define BLOCK_SIZE 1024
 
 int main(void) {
   char *line = NULL;
   size_t len = 0;
   int counter = 0;
 
-  printf("Enter the map directions\n");
-  if (getline(&line, &len, stdin) == -1)
-    perror("Error reading the stream input");
+  FILE *f;
+  char c;
 
-  while (*line != '\0') {
-    if (*line == '(') {
-      printf("Santa is at floor ++: %d\n", counter);
+  f = fopen("text.txt", "r");
+  if (f == NULL) {
+    perror("error:");
+    return (1);
+  }
+
+  while ((c = fgetc(f)) != EOF) {
+    if (c == '(') {
       ++counter;
-    } else if (*line == ')') {
-      printf("Santa is at floor --: %d\n", counter);
+    } else if (c == ')') {
       --counter;
     } else {
-      printf("Our culprit\n");
+      continue;
     }
-    line++;
   }
-  printf("Santa is at floor: %d\n", counter);
+  printf("santa is at %d\n", counter);
+
   return (0);
+  /** printf("Enter the map directions\n");
+   if (getline(&line, &len, stdin) == -1)
+     perror("Error reading the stream input");
+ **/
+
+  /**  while (*line != '\0') {
+      if (*line == '(') {
+        printf("Santa is at floor ++: %d\n", counter);
+        ++counter;
+      } else if (*line == ')') {
+        printf("Santa is at floor --: %d\n", counter);
+        --counter;
+      } else {
+        printf("Our culprit\n");
+      }
+      line++;
+    }
+    printf("Santa is at floor: %d\n", counter);
+    return (0);
+    **/
 }
