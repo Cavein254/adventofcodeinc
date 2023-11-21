fe034592cb52cde81cbf0f08cf5df33c79422bd9
johnmuthua <caveomuthama@gmail.com>
2023-11-21 17:26:00 +0300

    Input version using console

--- 2015/dayone.c
+++ 2015/dayone.c
@@ -4,18 +4,24 @@
 int main(void) {
   char *line = NULL;
   size_t len = 0;
+  int counter = 0;
 
   printf("Enter the map directions\n");
   if (getline(&line, &len, stdin) == -1)
     perror("Error reading the stream input");
 
   while (*line != '\0') {
-    if ((*line != ')') || (*line != '(')) {
-      perror("Not a valid map\n");
-      return (1);
+    if (*line == '(') {
+      printf("Santa is at floor ++: %d\n", counter);
+      ++counter;
+    } else if (*line == ')') {
+      printf("Santa is at floor --: %d\n", counter);
+      --counter;
+    } else {
+      printf("Our culprit\n");
     }
     line++;
   }
-
+  printf("Santa is at floor: %d\n", counter);
   return (0);
 }
